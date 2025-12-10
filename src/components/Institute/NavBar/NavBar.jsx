5a5ad1ba7fc3700a1b5551bb0a829eed0021835e
cecillwala <lwalacecil13@gmail.com>
2025-12-10 12:25:26 +0300

    fixed bug

--- src/components/Institute/NavBar/NavBar.jsx
+++ src/components/Institute/NavBar/NavBar.jsx
@@ -41,14 +41,14 @@ const Navbar = () => {
   };
 
   // Function to handle navigation to services section
-  const navigateToServices = (e) => {
-    if (location.pathname !== '/') {
-      // If not on homepage, navigate directly to services section on homepage
-      e.preventDefault();
-      window.location.href = '/#services';
-    }
-    // If already on homepage, default anchor behavior will work
-  };
+  // const navigateToServices = (e) => {
+  //   if (location.pathname !== '/') {
+  //     // If not on homepage, navigate directly to services section on homepage
+  //     e.preventDefault();
+  //     window.location.href = '/#services';
+  //   }
+  //   // If already on homepage, default anchor behavior will work
+  // };
 
   // const scrollToSection = (sectionId) => {
   //   const section = document.getElementById(sectionId);

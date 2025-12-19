e94b097b6f2b5e7f23db598c3f939c15079ec1fd
cecillwala <lwalacecil13@gmail.com>
2025-12-19 10:06:23 +0300

    font changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/NavBar/Navbar';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
 import Services from './components/Services';
+import './index.css';
 
 
 
